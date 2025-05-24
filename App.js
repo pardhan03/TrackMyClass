607c5f7e5e9656de483a52702e4301ccf2baa983
Manish Pardhan <manish@instaastro.in>
2025-05-24 12:30:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+function App(){
+
+  return (
+    <View>
+      <Text>TrackMyClass</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
